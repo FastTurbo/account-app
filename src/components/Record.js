e0b83b92c59e436a3442b8bfdb52f7d158b021f2
tal <tianpengfei1>
2018-07-11 17:00:18 +0800

    commit

---
+++ src/components/Record.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react'
+
+export default class Record extends Component{
+
+    render(){
+        return (
+            <tr>
+                <td>{this.props.record.date}</td>
+                <td>{this.props.record.title}</td>
+                <td>{this.props.record.amount}</td>
+            </tr>
+        )
+    }
+}
